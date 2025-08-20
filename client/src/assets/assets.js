d49be206e7a5be7b188866ea808584b4ab764360
duythinh26 <pduythinh79@gmail.com>
2025-08-20 10:23:57 +0700

    create sidebar for main layout

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -16,11 +16,11 @@ export const assets = {
 }
 
 export const menuItemsData = [
-    { to: '/', label: 'Feed', Icon: Home },
-    { to: '/messages', label: 'Messages', Icon: MessageCircle },
-    { to: '/connections', label: 'Connections', Icon: Users },
-    { to: '/discover', label: 'Discover', Icon: Search },
-    { to: '/profile', label: 'Profile', Icon: UserIcon },
+    { to: '/', label: 'Bảng tin', Icon: Home },
+    { to: '/messages', label: 'Tin nhắn', Icon: MessageCircle },
+    { to: '/connections', label: 'Kết nối', Icon: Users },
+    { to: '/discover', label: 'Khám phá', Icon: Search },
+    { to: '/profile', label: 'Trang cá nhân', Icon: UserIcon },
 ];
 
 export const dummyUserData = {
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { viVN } from '@clerk/localizations'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} localization={viVN}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
