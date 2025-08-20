72952fbea6470c6e35db1b9cbb97d5f49d63b205
duythinh26 <pduythinh79@gmail.com>
2025-08-20 21:54:45 +0700

    create newsfeed for stories section

--- client/src/pages/NewsFeed.jsx
+++ client/src/pages/NewsFeed.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loading from '../components/Loading'
+import StoriesBar from '../components/StoriesBar'
+import { dummyPostsData } from '../assets/assets'
 
 const NewsFeed = () => {
-    return (
-        <div>NewsFeed</div>
+
+    const [newsFeeds, setNewsFeeds] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const fetchNewsFeeds = async () => {
+        setNewsFeeds(dummyPostsData)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchNewsFeeds()
+    }, [])
+
+    return !loading ? (
+        <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+            {/* Stories and post list */}
+            <div>
+                <StoriesBar />
+                <div className='p-4 space-y-6'>
+                    Danh sách bài viết
+                </div>
+            </div>
+
+            {/* Right Sidebar */}
+            <div>
+                <div className="">
+                    <h1 className="">Nhà tài trợ</h1>
+                </div>
+                <h1 className="">Tin nhắn gần đây</h1>
+            </div>
+        </div>
+    ) : (
+        <Loading />
     )
 }
 
