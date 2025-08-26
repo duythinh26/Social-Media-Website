cd5b9b39c5e59340c4d14b472ca823b8a33a27e5
duythinh26 <pduythinh79@gmail.com>
2025-08-26 13:19:22 +0700

    modify api location route

--- server/server.js
+++ server/server.js
@@ -14,11 +14,12 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get('/', (req, res) => {
     res.send("Server is running");
 })
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
