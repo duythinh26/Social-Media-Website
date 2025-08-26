61dde3c4e8c30cc10c304bcd83be97d8e871eaf6
duythinh26 <pduythinh79@gmail.com>
2025-08-26 13:58:14 +0700

    setup clerk for verifying

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { serve } from "inngest/express";
 import { inngest, functions } from './inngest/index.js';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
@@ -13,6 +14,7 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
