a7703da13a7a222db82f2017dcc4e2dee118cc17
duythinh26 <pduythinh79@gmail.com>
2025-08-26 10:23:10 +0700

    setup inngest webhook

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { serve } from "inngest/express";
+import { inngest, functions } from './inngest/index.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
@@ -12,6 +17,8 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("Server is running");
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
